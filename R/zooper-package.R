7ffe3dbef764a174f2c63d17346aaf1045c7fa81
claireestey <esteyc@mcmaster.ca>
2024-11-13 14:56:37 -0500

    Initial commit

---
+++ R/zooper-package.R
@@ -0,0 +1,17 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+#' zoopl: A package to test if renv is my issue
+#'
+#' This package is an exercise in checking if renv() is what is causing my
+#' issues with pkgdown for GEOG 712.
+#'
+#' @docType package
+#' @name zooper
+#' @author Claire, McMaster University \email{esteyc@@mcmaster.ca}
+#' @references \url{https://github.com/claireestey/zooper}
+NULL
---
+++ R/zoopl.R
@@ -0,0 +1,14 @@
+#' Test function
+#'
+#' @name zoopl
+#' @param a A number
+#' @param b A number
+#' @return The product of \code{a} and \code{b}
+#' @export
+#' @examples
+#' zoopl(2, 5)
+#' zoopl(4, 8)
+
+zoopl <- function(a = 1, b = 2){
+  a * b
+}
